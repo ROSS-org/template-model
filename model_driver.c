c3d6ab8cfc2ce95bb96561858fe9a021c1b7c2f1
Elsa Gonsiorowski (Eris) <gonsie@me.com>
2015-08-13 11:24:52 -0700

    whitespace

--- model_driver.c
+++ model_driver.c
@@ -6,7 +6,7 @@
 // - a reverse event function
 // - a finalization function
 // - all custom mapping functions (if needed)
-// - the LP type definition 
+// - the LP type definition
 
 //Includes
 #include <stdio.h>
@@ -135,32 +135,32 @@ void model_custom_mapping(void){
   int nlp_per_kp;
   int lp_id, kp_id;
   int i, j;
-  
+
   // nlp should be divisible by nkp (no wasted LPs)
   nlp_per_kp = ceil((double) g_tw_nlp / (double) g_tw_nkp);
   if (!nlp_per_kp) tw_error(TW_LOC, "Not enough KPs defined: %d", g_tw_nkp);
-  
+
   //gid of first LP on this PE (aka node)
   g_tw_lp_offset = g_tw_mynode * g_tw_nlp;
-  
+
 #if VERIFY_MAPPING
   prinf("NODE %d: nlp %lld, offset %lld\n", g_tw_mynode, g_tw_nlp, g_tw_lp_offset);
 #endif
-  
+
   // Loop through each PE (node)
   for (kp_id = 0, lp_id = 0, pe = NULL; (pe = tw_pe_next(pe)); ) {
-    
+
     // Define each KP on the PE
     for (i = 0; i < nkp_per_pe; i++, kp_id++) {
-      
+
       tw_kp_onpe(kpid, pe);
-      
+
       // Define each LP on the KP
       for (j = 0; j < nlp_per_kp && lp_id < g_tw_nlp; j++, lp_id++) {
-	
+
 	tw_lp_onpe(lp_id, pe, g_tw_lp_offset + lp_id);
 	tw_lp_onkp(g_tw_lp[lp_id], g_tw_kp[kp_id]);
-	
+
 #if VERIFY_MAPPING
 	if (0 == j % 20) { // print detailed output for only some LPs
 	  printf("PE %d\tKP %d\tLP %d\n", pe->id, kp_id, (int) lp_id + g_tw_lp_offset);
@@ -169,12 +169,12 @@ void model_custom_mapping(void){
       }
     }
   }
-  
+
   //Error checks for the mapping
   if (!g_tw_lp[g_tw_nlp - 1]) {
     tw_error(TW_LOC, "Not all LPs defined! (g_tw_nlp=%d)", g_tw_nlp);
   }
-  
+
   if (g_tw_lp[g_tw_nlp - 1]->gid != g_tw_lp_offset + g_tw_nlp - 1) {
     tw_error(TW_LOC, "LPs not sequentially enumerated");
   }
@@ -206,4 +206,3 @@ tw_lptype model_lps[] = {
     sizeof(state)
   },
   { 0 },
-};
